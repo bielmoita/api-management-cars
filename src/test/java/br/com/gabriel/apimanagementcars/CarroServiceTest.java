399584eb7b077a465b0c7024da0f1c3d8b435138
Gabriel Fernando <Biel@MacBook-Pro-de-Melanie.local>
2024-03-01 15:57:29 -0300

    teste incompleto

--- src/test/java/br/com/gabriel/apimanagementcars/CarroServiceTest.java
+++ src/test/java/br/com/gabriel/apimanagementcars/CarroServiceTest.java
@@ -2,24 +2,22 @@ package br.com.gabriel.apimanagementcars;
 
 import br.com.gabriel.apimanagementcars.models.Carro;
 import br.com.gabriel.apimanagementcars.services.CarroService;
-import org.junit.Rule;
-import org.junit.jupiter.api.Test;
-import org.junit.rules.ExpectedException;
-import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
 public class CarroServiceTest {
 
     private CarroService carroService;
 
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
 
 
     @Test
     public void testeCarro() throws Exception{
 
         Carro carro = new Carro();
-        string id = carro.getId();
+        carroService.create(carro);
+        long id = carro.getId();
         assertEquals(1, id);
 
 
